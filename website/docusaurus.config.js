926a990f4a39b5a8d539a928607bc95774a0cf7b
Jacob Maidens <mail@jacobmaidens.com>
2022-10-13 21:41:44 +0100

    test

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://jmaidensgd.github.io',
-  baseUrl: '/',
+  baseUrl: '/JMaidensPortfolio/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
