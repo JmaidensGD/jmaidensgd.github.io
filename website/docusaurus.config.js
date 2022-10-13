8a889bc6072c1279f1d98bd6405c6bb198178ac6
Jacob Maidens <mail@jacobmaidens.com>
2022-10-13 22:12:31 +0100

    project name

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,16 +9,17 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://jmaidensgd.github.io',
-  baseUrl: '',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   trailingSlash: false,
+  deploymentBranch: 'gh-pages',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'JMaidensGD', // Usually your GitHub org/user name.
-  projectName: 'JMaidensPortfolio', // Usually your repo name.
+  projectName: 'jmaidensgd.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
